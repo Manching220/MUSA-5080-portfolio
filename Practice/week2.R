5bcd4b5fc5ab176b9632b8daeeabca68640c4559
Manching H. <manching0220@163.com>
2026-09-25 07:53:16 -0400

    assignment3

---
+++ Practice/week2.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(tidycensus)
+
+#I'm going to load some data now!
+pa_income <- get_acs(
+  geography = "county",
+  variables = "B19013_001",
+  state = "PA",
+  year = 2023,
+  survey = "acs5"
+)
+
+dim(pa_income)
+glimpse(pa_income)
+head(pa_income,10)
+
+dim(pa_incomeS)
+dim(pa_income)
